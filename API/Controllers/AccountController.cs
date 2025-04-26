ae90f5e22ed581ccdc2e67a07832409cb0d6e395
aburgy-93 <aburgy@uwalumni.com>
2025-04-26 17:54:58 -0500

    Added JWT Tokens

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController(DataContext context, ITokenService tokenService) 
+    : BaseApiController
+{
+    [HttpPost("register")] // account/register
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+    {
+        if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+
+        using var hmac = new HMACSHA512();
+
+        var user = new AppUser
+        {
+            UserName = registerDto.Username.ToLower(),
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+            PasswordSalt = hmac.Key
+        };
+
+        // Add user to the context/DB
+        context.Users.Add(user);
+
+        // Await the saved changes
+        await context.SaveChangesAsync();
+
+        return new UserDto
+        {
+            Username = user.UserName,
+            Token = tokenService.CreateToken(user)
+        };
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+    {
+        // Get user if exists, or return null 
+        var user = await context.Users.FirstOrDefaultAsync(x => 
+            x.UserName == loginDto.Username.ToLower());
+
+        if (user == null) return Unauthorized("Invalid username");
+
+        // Use the key (PasswordSalt)
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        // Compute the hash
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        // Compare the computed hash with the hash in the DB
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+        }
+
+        return new UserDto
+        {
+            Username = user.UserName,
+            Token = tokenService.CreateToken(user)
+        };
+    }
+
+    private async Task<bool> UserExists(string username)
+    {
+        return await context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower()); // Bob!= bob
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,18 +1,11 @@
-using API.Data;
-using Microsoft.EntityFrameworkCore;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-
-// Adding our connection to the DB to the services and registering the connection string
-builder.Services.AddDbContext<DataContext>(opt => 
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -28,10 +21,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
     .WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
